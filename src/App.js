82cab74f857e6d6b953d6ac2b933da2f934dc4db
irenetjrs <irkapidgorodetska@gmail.com>
2023-03-20 12:53:38 +0200

    tasks solved

--- src/App.js
+++ src/App.js
@@ -1,42 +1,91 @@
 import React, { useState, useEffect } from "react";
 
   function App () {
-  const [coffe, setCoffe] = useState(0);
-  const [sugar, setSugar] = useState(0);
+  const [products, setProducts] = useState({
+    coffe: 0,
+    sugar: 0
+  });
+  // const [coffe, setCoffe] = useState(0);
+  // const [sugar, setSugar] = useState(0);
 
-const addCoffe = () => setCoffe(coffe + 1);
-const addSugar = () => setSugar(sugar + 1);
+const addCoffe = () => setProducts((prevState) => {
+  return {
+    ...prevState,
+    coffe: prevState.coffe + 1
+  }
+});
+const addSugar = () => setProducts((prevState) => {
+  return {
+    ...prevState,
+    sugar: prevState.sugar + 1
+  }
+});
+const removeCoffe = () => setProducts((prevState) => {
+  return {
+    ...prevState,
+    coffe: prevState.coffe - 1
+  }
+});
+const removeSugar = () => setProducts((prevState) => {
+  return {
+    ...prevState,
+    sugar: prevState.sugar - 1
+  }
+});
 
 const save = () => {
-  localStorage.setItem('coffe', coffe);
-  localStorage.setItem('sugar', sugar);
+  localStorage.setItem('coffe', products.coffe);
+  localStorage.setItem('sugar', products.sugar);
 }
 
 const clear = () => {
     localStorage.removeItem('coffe');
     localStorage.removeItem('sugar');
-    setCoffe(0);
-    setSugar(0);
+    setProducts({
+      coffe: 0,
+      sugar: 0
+    });
 }
 
-useEffect(()=>{
+const [showCoffe, setShowCoffe] = useState(true);
+const [showSugar, setShowSugar] = useState(true);
+
+
+useEffect(() => {
   if (localStorage.getItem('coffe')){
-    setCoffe(+localStorage.getItem('coffe'));
-    setSugar(+localStorage.getItem('sugar'));
-  }
-}, []);
+    setProducts((prevState) => ({
+      ...prevState,
+      coffe: +localStorage.getItem('coffe'),
+      sugar: +localStorage.getItem('sugar')
+    }))
+  }});
+
+useEffect(() => {
+  setShowCoffe(products.coffe > 0);
+  setShowSugar(products.sugar > 0)
+}, [products]);
 
 return (
   <div className="wrapper">
     <div className="list">
       <h1>Product list</h1>
       <div className='product'>
-      <span>{`Coffe: ${coffe}`}</span>
+      <span>{`Coffe: ${products.coffe}`}</span>
       <button onClick={addCoffe}>Add</button>
+      {showCoffe && (
+        <button onClick={removeCoffe} disabled={products.coffe === 0}>
+          Remove
+          </button>
+      )}
       </div>
       <div className='product'>
-      <span>{`Sugar: ${sugar}`}</span>
+      <span>{`Sugar: ${products.sugar}`}</span>
         <button onClick={addSugar}>Add</button>
+        {showSugar && (
+        <button onClick={removeSugar} disabled={products.sugar === 0}>
+          Remove
+        </button>
+        )}
       </div>
       <div className='save'>
           <button onClick={save}>SAVE</button>
